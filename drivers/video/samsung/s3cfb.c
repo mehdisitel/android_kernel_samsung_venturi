63c4dc103cddbaae4eea686cddada549d5c543e1
jung-min.oh <jung-min.oh@samsung.com>
2011-08-06 11:06:15 +0900

    S5PC11X: LCD: Add new config to use multiple windows
    Add the following configs in consideration with using multiple
    windows for video rendering as overlay. They are used in calculation
    of FIMD reserved memory.
    - FB_S3C_NUM_OVLY_WIN : the number of overlay windows for video
    rendering
    - FB_S3C_NUM_BUF_OVLY_WIN : the number of framebuffers for overlay
    windows
    
    Change-Id: Id37b426a555d8742b9156fce0c398c10a68cd350
    Signed-off-by: jung-min.oh <jung-min.oh@samsung.com>
    
    Conflicts:
    	drivers/video/samsung/s3cfb.c

--- drivers/video/samsung/s3cfb.c
+++ drivers/video/samsung/s3cfb.c
@@ -82,6 +82,10 @@ static struct struct_frame_buf_mark  frame_buf_mark = {
 	.frames = 2
 };
 
+#if (CONFIG_FB_S3C_NUM_OVLY_WIN >= CONFIG_FB_S3C_DEFAULT_WINDOW)
+#error "FB_S3C_NUM_OVLY_WIN should be less than FB_S3C_DEFAULT_WINDOW"
+#endif
+
 struct s3c_platform_fb *to_fb_plat(struct device *dev)
 {
 	struct platform_device *pdev = to_platform_device(dev);
