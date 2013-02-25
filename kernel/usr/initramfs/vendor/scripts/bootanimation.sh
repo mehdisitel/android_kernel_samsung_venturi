cb56bb3abd7e56992f30342679a2db72f91c0022
JackpotClavin <jonclavin@gmail.com>
2013-02-25 17:09:48 -0500

    Remove ramdisk from working directory.

--- kernel/usr/initramfs/vendor/scripts/bootanimation.sh
+++
@@ -1,6 +0,0 @@
-#!/sbin/sh
-if [ -f /system/media/bootanimation.zip ]; then
-  /vendor/bootanimation
-else
-  /system/bin/samsungani
-fi;
--- kernel/usr/initramfs/vendor/scripts/run-parts.sh
+++
@@ -1,4 +0,0 @@
-#!/sbin/sh
-export PATH=/system/bin:/sbin:/system/xbin:/bin
-log "running init.d scripts with run-parts"
-/system/bin/logwrapper run-parts /system/etc/init.d
